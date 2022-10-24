dc95161928d5857858c2def55e2ff98141c79ce2
Rogerio Pio <rogerioxpio@gmail.com>
2022-10-24 14:04:36 -0300

    Removendo tarefas

--- routes/tasksRoutes.js
+++ routes/tasksRoutes.js
@@ -4,4 +4,7 @@ export const router = express.Router();
 
 router.get('/add', TaskController.createTask);
 router.post('/add', TaskController.createTaskSave);
+
+router.post('/remove', TaskController.removeTask);
+
 router.get('/', TaskController.showTasks);
